cfa00bb19388386187a8ebde82ee408286750823
ROMANVIKI <vikiadhi2016@gmail.com>
2024-12-27 19:46:38 +0530

    changing allowed hosts

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-tu21zdmdq0q*e4q#6&&k-1)!fh7vub2l^!@3n^53dskdo)wp&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
