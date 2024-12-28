708cf0c67bb3fbf62ff102af83ce0d58cc54fae9
ROMANVIKI <vikiadhi2016@gmail.com>
2024-12-28 17:28:18 +0530

    added rud view

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import ListDataTableView
-
+from .views import ListDataTableView, RetrieveUpdateDestroyDataTableAPIView
 
 
 urlpatterns = [
-    path('list-data/', ListDataTableView.as_view(), name="list-data"),
+    path("list-data/", ListDataTableView.as_view(), name="list-data"),
+    path(
+        "rud-data/<str:code>",
+        RetrieveUpdateDestroyDataTableAPIView.as_view(),
+        name="rud-data",
+    ),
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from .serializers import DataTableSerializer
 from .models import DataTable
 from rest_framework.permissions import IsAdminUser, AllowAny
 
 
-
-
-
-
 class ListDataTableView(ListAPIView):
     queryset = DataTable.objects.all()
-    serializer_class =  DataTableSerializer
+    serializer_class = DataTableSerializer
+    permission_classes = [AllowAny]
+
+
+class RetrieveUpdateDestroyDataTableAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = DataTable.objects.all()
+    serializer_class = DataTableSerializer
     permission_classes = [AllowAny]
